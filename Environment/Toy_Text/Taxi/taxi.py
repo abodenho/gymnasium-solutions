19b9ef126833889d4241b01654f002b80aed0c44
abodenho <45141024+abodenho@users.noreply.github.com>
2026-07-13 20:22:53 +0200

    [ADD] taxi

---
+++ Environment/Toy_Text/Taxi/taxi.py
@@ -0,0 +1,30 @@
+import gymnasium as gym
+from Models.Reinforcement_Learning.q_learning import QLearningAgent,Q_TABLE,EPSILON_STRATEGY
+from Environment.Toy_Text.template_toy_text import *
+
+env_train = gym.make('Taxi-v4')
+
+
+env_test = gym.make('Taxi-v4',render_mode="human")
+
+
+
+NUMBER_EPISODE = 100000
+NUMBER_TEST = 100
+
+ALPHA = 0.1
+GAMMA = 0.999
+
+EPSILON = 0.80
+EPSILON_MODE = EPSILON_STRATEGY.DECAY
+EPSILON_MIN = 0.1
+EPSILON_DECAY = 0.9
+
+MEMORY_MODE = Q_TABLE.STATIC
+NUMBER_ACTION = len([*range(env_train.action_space.n)])
+NUMBER_STATE = int(env_train.observation_space.n)
+
+Agent = QLearningAgent(ALPHA,GAMMA,EPSILON,MEMORY_MODE,EPSILON_MODE,EPSILON_MIN,EPSILON_DECAY,NUMBER_ACTION,NUMBER_STATE)
+
+
+play_game(env_train,env_test,Agent,NUMBER_EPISODE,NUMBER_TEST)
